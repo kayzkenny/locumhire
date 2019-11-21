e2ebf7207697ed84c09b811b5bdd0bfdcba602a8
Kehinde Akeredolu <kayzkenny@gmail.com>
2019-11-21 12:33:06 +0100

    added vuetify
    added vuetify plugin with material design icons and set primary and secondary colors.

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,18 @@
+import Vue from "vue";
+import Vuetify from "vuetify/lib";
+
+Vue.use(Vuetify);
+
+export default new Vuetify({
+  icons: {
+    iconfont: "mdi"
+  },
+  theme: {
+    themes: {
+      light: {
+        primary: "#39cdfa",
+        secondary: "#8f7fa5"
+      }
+    }
+  }
+});
